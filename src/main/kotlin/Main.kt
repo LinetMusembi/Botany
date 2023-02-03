9c3eb88025a3efb916c3c5018fcc94395dc04036
linetmusembi <musembilinetmwongeli@gmail.com>
2023-02-03 17:26:54 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var name = arrayOf ("Linet-Musembi","Ann-Musembi")
+    println(name)
+    var age = arrayOf ("19","20")
+    println(age)
+    var weight = arrayOf ("48kgs","36kgs")
+
+}
